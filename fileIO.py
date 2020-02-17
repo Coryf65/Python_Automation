c7dcb0c73bf2cda7a3ea07a6527113ec018e4521
Coryf65 <coryf65@gmail.com>
2020-02-16 19:39:47 -0600

    only print the line if the user passed the test

--- fileIO.py
+++ fileIO.py
@@ -3,8 +3,14 @@
 # open the file with read permissions
 f = open('inputFile.txt', 'r')
 
-# read the file
-print(f.read())
+count = 0
+# now we are tasked to only get the ones who passed
+for line in f:
+    # each data point is using space as a delimeter
+    line_split = line.split()
+    # print the line if = P
+    if line_split[2] == 'P':
+        # print the line
+        print(line)
 
-# print the contents to the terminal
 f.close()
