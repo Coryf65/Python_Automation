6b71c97285b93b3ccb100ad0ecf00a04a9584e79
Coryf65 <coryf65@gmail.com>
2020-02-16 19:22:16 -0600

    reading the file "inputFile" and out put contents to terminal

--- fileIO.py
+++ fileIO.py
@@ -1,7 +1,10 @@
 # Automation with Python
 
+# open the file with read permissions
 f = open('inputFile.txt', 'r')
 
+# read the file
 print(f.read())
 
+# print the contents to the terminal
 f.close()
