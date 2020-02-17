efb592de0e2dfb7200e4cd68ec430ebc3831b2f4
Coryf65 <coryf65@gmail.com>
2020-02-16 20:05:00 -0600

    this script will organze and create folders by file types

--- organize/organize.py
+++ organize/organize.py
@@ -1,6 +1,36 @@
+# Organzing Files
+import os
+from pathlib import Path
+
+# Dictionary
 SUBDIRECTORIES = {
     "DOCUMENTS": ['.pdf','.rtf','.txt'],
     "AUDIO": ['.m4a','.m4b','.mp3'],
     "VIDEOS": ['.mov','.avi','.mp4'],
     "IMAGES": ['.jpg','.jpeg','.png']
 }
+
+# Finds which Directory to put the File into
+def pickDirectory(value):
+    for category, suffixes in SUBDIRECTORIES.items():
+        for suffix in suffixes:
+            if suffix == value:
+                return category
+    # could not find the category
+    return 'MISC'
+
+# testing the function
+# print(pickDirectory('.mp3'))
+
+def organizeDirectory():
+    for item in os.scandir():
+        if item.is_dir():
+            continue
+        filePath = Path(item)
+        fileType = filePath.suffix.lower()
+        directory = pickDirectory(fileType)
+        directoryPath = Path(directory)
+        # if the directory does not exist create one
+        if directoryPath.is_dir() != True:
+            directoryPath.mkdir()
+        filePath.rename(directoryPath.joinpath(filePath))
