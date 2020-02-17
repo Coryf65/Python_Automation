ef6ccbc23ec4e08d2a4623075336e106347554b9
Coryf65 <coryf65@gmail.com>
2020-02-16 19:43:20 -0600

    create a file 'PassFile.txt' to store all the Passes

--- fileIO.py
+++ fileIO.py
@@ -1,16 +1,21 @@
 # Automation with Python
 
+# now learning how to write files
+
 # open the file with read permissions
 f = open('inputFile.txt', 'r')
 
-count = 0
+# creates a new file
+passFile = open('PassFile.txt', 'w')
+
 # now we are tasked to only get the ones who passed
 for line in f:
     # each data point is using space as a delimeter
     line_split = line.split()
     # print the line if = P
     if line_split[2] == 'P':
-        # print the line
-        print(line)
+        # write to the file
+        passFile.write(line)
 
 f.close()
+passFile.close()
